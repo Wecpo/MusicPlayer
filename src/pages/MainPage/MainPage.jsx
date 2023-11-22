acc419cf8e9f0752884c3fd608a2106dadcf4c50
Senin Maksim <wecpo@yandex.ru>
2023-11-22 21:11:26 +0300

    developed tracks search query

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,44 @@
+import { useState } from "react";
 import trackList from "../../assets/trackList";
 import Track from "../../components/Track/Track";
 import styles from "./mainPage.module.scss";
+import { Input } from "@mui/material";
 
-const MainPage = () => (
-  <div className={styles.search}>
-    <>Track search</>
-    <div className={styles.list}>
-      {trackList.map((track) => (
-        <Track key={track.id} track={track} />
-      ))}
+const runSearch = (query) => {
+  if (!query) {
+    return trackList;
+  }
+
+  const lowerCaseQuery = query.toLowerCase();
+
+  return trackList.filter(
+    (track) =>
+      track.title.toLowerCase().includes(lowerCaseQuery) ||
+      track.artists.toLowerCase().includes(lowerCaseQuery)
+  );
+};
+
+const MainPage = () => {
+  const [tracks, setTracks] = useState(trackList);
+
+  const handleChange = (event) => {
+    const foundTracks = runSearch(event.target.value);
+    setTracks(foundTracks);
+  };
+
+  return (
+    <div className={styles.search}>
+      <Input
+        className={styles.input}
+        placeholder="Search track"
+        onChange={handleChange}
+      />
+      <div className={styles.list}>
+        {tracks.map((track) => (
+          <Track key={track.id} track={track} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default MainPage;
