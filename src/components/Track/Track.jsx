3ce23a2b8931b0ffd2e91b2f65c760d259d5cf97
Senin Maksim <wecpo@yandex.ru>
2023-11-23 19:35:57 +0300

    developed play/pause button, audio context

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,17 +1,25 @@
 import styles from "./track.module.scss";
 import { IconButton } from "@mui/material";
-import { PlayArrow } from "@mui/icons-material";
+import { PlayArrow, Pause } from "@mui/icons-material";
 import secondsToMMSS from "../../utils/secondsToMMSS";
+import { useContext } from "react";
+import { AudioContext } from "../../context/AudioContext";
+import classNames from "classnames";
 
 const Track = ({ track }) => {
   const { id, src, title, preview, artists, duration } = track;
 
   const formattedDuration = secondsToMMSS(duration);
 
+  const { currentTrack, isPlaying, handleToggleAudio } =
+    useContext(AudioContext);
+
+  const isCurrentTrack = currentTrack.id === id;
+
   return (
-    <div className={styles.track}>
-      <IconButton>
-        <PlayArrow />
+    <div className={classNames(styles.track, isCurrentTrack && styles.playing)}>
+      <IconButton onClick={() => handleToggleAudio(track)}>
+        {isCurrentTrack && isPlaying ? <Pause /> : <PlayArrow />}
       </IconButton>
       <img className={styles.preview} src={preview} alt="preview" />
       <div className={styles.credits}>
